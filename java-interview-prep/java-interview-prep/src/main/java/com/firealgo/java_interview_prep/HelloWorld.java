841eb4c39ae374e08dc5a04d0ee8dfad8e1ea237
Deepak <deepakguptaoptimistic@gmail.com>
2024-06-08 12:05:28 +0100

    +Adding new repo java-interview-prep

---
+++ java-interview-prep/java-interview-prep/src/main/java/com/firealgo/java_interview_prep/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.firealgo.java_interview_prep;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ java-interview-prep/java-interview-prep/src/test/java/com/firealgo/java_interview_prep/JavaInterviewPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firealgo.java_interview_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInterviewPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
