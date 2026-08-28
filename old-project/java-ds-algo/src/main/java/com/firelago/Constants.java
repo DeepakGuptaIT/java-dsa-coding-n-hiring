07601f5b80a291730568f4c9184a52d0331013c1
Deepak <deepakguptaoptimistic@gmail.com>
2026-08-28 19:13:10 +0530

    deleted all repository

--- old-project/java-ds-algo/src/main/java/com/firelago/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.firelago;
-
-public class Constants {
-	public enum Gender {
-		MALE, FEMALE
-	}
-
-}
--- old-project/java-ds-algo/src/main/java/com/firelago/rivision/BinarySearchTree2.java
+++
@@ -1,5 +0,0 @@
-package com.firelago.rivision;
-
-public class BinarySearchTree2 {
-
-}
